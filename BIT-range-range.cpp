e3562911c21ce551b077efa30db4d2a1d3ace06b
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2018-11-29 12:47:15 +0100

    More reorganizing

--- BIT-range-range.cpp
+++ BIT-range-range.cpp
@@ -1,7 +1,6 @@
 int sum(int x, int BITTree1[], int BITTree2[]) {
   return getSum(BITTree1, x) * x - getSum(BITTree2, x);
 }
-
 void updateRange(int BITTree1[], int BITTree2[], int n, int val,
                  int l, int r) {
   updateBIT(BITTree1, n, l, val);
@@ -9,7 +8,6 @@ void updateRange(int BITTree1[], int BITTree2[], int n, int val,
   updateBIT(BITTree2, n, l, val * (l - 1));
   updateBIT(BITTree2, n, r + 1, -val * r);
 }
-
 int rangeSum(int l, int r, int BITTree1[], int BITTree2[]) {
   return sum(r, BITTree1, BITTree2) -
          sum(l - 1, BITTree1, BITTree2);
